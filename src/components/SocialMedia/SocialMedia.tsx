7c7c51aae096f511bb5a45ba51cc115ba0150ef5
Andrei Toledo <actoledo.andrei@gmail.com>
2025-10-23 13:13:01 -0300

    chore: update for netfly deploy

--- src/components/SocialMedia/SocialMedia.tsx
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
-import { Container, SocialIcon } from './styles';
+import React from "react";
+import { Container, SocialIcon } from "./styles";
 import { SlSocialInstagram, SlSocialSpotify } from "react-icons/sl";
+import { IconType } from "react-icons";
 
-const SocialMedia:React.FC = () => {
-    const socials = [
-        { href: "https://instagram.com/toledoandrei", icon: <SlSocialInstagram />, title: "Instagram" },
-        { href: "https://open.spotify.com/user/12174950077?si=ad4e5e0383f64de3", icon: <SlSocialSpotify />, title: "Spotify" }
-    ]
-    return (
-        <Container>
-            {socials.map((social, index) => (
-                <SocialIcon href={social.href} key={'social' + index} title={social.title}> {social.icon} </SocialIcon>
-            ))}
-        </Container>
-    );
+interface SocialLink {
+  href: string;
+  icon: IconType;
+  title: string;
+}
+
+const SocialMedia: React.FC = () => {
+  const socials: SocialLink[] = [
+    {
+      href: "https://instagram.com/toledoandrei",
+      icon: SlSocialInstagram,
+      title: "Instagram",
+    },
+    {
+      href: "https://open.spotify.com/user/12174950077?si=ad4e5e0383f64de3",
+      icon: SlSocialSpotify,
+      title: "Spotify",
+    },
+  ];
+  return (
+    <Container>
+      {socials.map(({ href, icon: Icon, title }) => (
+        <SocialIcon href={href} key={title} title={title}>
+          {" "}
+          {/* @ts-ignore */}
+          <Icon />{" "}
+        </SocialIcon>
+      ))}
+    </Container>
+  );
 };
 
 export default SocialMedia;
