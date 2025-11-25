ece9d0e934d5d33339023ee0dcaccba0571e5a0c
Andrei Toledo <actoledo.andrei@gmail.com>
2025-11-24 21:51:52 -0300

    feat: add title and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
 import "./tailwind.css";
 import "./globals.css";
 import { Toaster } from "sonner";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Konn",
+  description:
+    "Crie sua página de links personalizada, estilize do seu jeito e compartilhe com o mundo.",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.png",
+    apple: "/apple-touch-icon.png",
+  },
+};
 
 export default function RootLayout({
   children,
